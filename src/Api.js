4b3cb66673b18c3136aba59d2aeec31fdac105a8
Mohamed-Yassine-Youssef <yousssmed@gmail.com>
2022-10-23 15:56:44 +0100

    App commit

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b85f659244mshfad9c318f3f96f4p12defcjsn6a9835c6e50f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "468522e89617754f432eb556f8329df1";
